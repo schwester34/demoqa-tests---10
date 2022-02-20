2e5477f5a3d3e51fc2b1a03fce6cf0ea3d24764b
schwester34 <yana64604@gmail.com>
2022-02-20 20:14:07 +0500

    added FormTest.java

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -11,7 +11,6 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class RegistrationPage {
     private final CalendarComponent calendarComponent = new CalendarComponent();
-
     private final SelenideElement
             pageHeader = $(".main-header");
     private final SelenideElement firstNameInput = $("#firstName");
@@ -30,95 +29,66 @@ public class RegistrationPage {
     public void openPage() {
         open("/automation-practice-form");
         pageHeader.shouldHave(text("Practice Form"));
-
     }
-
     public RegistrationPage setFirstName(String firstName) {
         firstNameInput.setValue(firstName);
-
         return this;
     }
-
     public RegistrationPage setLastName(String lastName) {
         lastNameInput.setValue(lastName);
-
         return this;
     }
-
     public RegistrationPage setEmail(String email) {
         emailInput.setValue(email);
-
         return this;
     }
-
     public RegistrationPage setGender(String gender) {
         $(byText(gender)).click();
-
         return this;
     }
-
     public RegistrationPage setMobileNumber(String number) {
         mobileNumberInput.setValue(number);
-
         return this;
     }
-
     public RegistrationPage setBirthDate(String day, String month, String year) {
         birthDatePicker.click();
         calendarComponent.setBirthDate(day, month, year);
-
         return this;
     }
-
     public RegistrationPage selectSubject(String subject) {
         subjectsDropdown.sendKeys("a");
         $(byText(subject)).click();
-
         return this;
     }
-
     public RegistrationPage selectHobbies(String hobbyOne, String hobbyTwo) {
         $(byText(hobbyOne)).click();
         $(byText(hobbyTwo)).click();
-
         return this;
     }
-
     public RegistrationPage uploadImage(String fileName) {
         chooseFileButton.uploadFromClasspath(fileName);
-
         return this;
     }
-
     public RegistrationPage setCurrentAddress(String address) {
         currentAddressInput.setValue(address);
-
         return this;
     }
-
     public RegistrationPage selectState(String state) {
         stateDropdown.scrollTo().click();
         $(byText(state)).click();
-
         return this;
     }
-
     public RegistrationPage selectCity(String city) {
         cityDropdown.click();
         $(byText(city)).click();
-
         return this;
     }
-
     public void submitForm() {
         submitButton.click();
-
     }
-
     public RegistrationPage verifyForm(String label, String value) {
         filledFormModal.shouldBe(visible);
         filledFormModal.$(byText(label)).parent().shouldHave(text(value));
-
         return this;
     }
 }
--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -4,10 +4,10 @@ import static com.codeborne.selenide.Selenide.$;
     public class CalendarComponent {
 
         public void setBirthDate(String day, String month, String year) {
+            $(".react-datepicker__day--0" + day);
             $(".react-datepicker__month-select").selectOption(month);
             $(".react-datepicker__year-select").selectOption(year);
-            $("[aria-label$='" + month + " " + day + "th, " + year + "']").click();
-            //$(byText(day)).click();import static com.codeborne.selenide.Selectors.byText;
+            $(".react-datepicker__day--0" + day).click();
         }
     }
 

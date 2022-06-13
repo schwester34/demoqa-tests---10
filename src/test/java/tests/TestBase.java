906b80013a6a15e5c34cbfc46b4c00bcb3e983ab
schwester34 <yana64604@gmail.com>
2022-06-13 17:54:19 +0500

    added owner example

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,21 +2,36 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import helpers.Attach;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
 
 public class TestBase {
     @BeforeAll
     static void setUp (){
+        CredentialsConfig credentialConfig = ConfigFactory.create(CredentialsConfig.class);
+        String selenoidLogin = credentialConfig.login();
+        String selenoidPassword = credentialConfig.password();
+
+        String selenoidURL = System.getProperty("selenoidURL");
+        System.out.println(selenoidURL);
+        String selenoidConnectionString = String.format("https://%s:%s@%s/wd/hub",
+                selenoidLogin,
+                selenoidPassword,
+                selenoidURL);
         SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.holdBrowserOpen = true;
+
+        Configuration.remote = selenoidConnectionString;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
@@ -29,6 +44,7 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
+        closeWebDriver();
 
     }
 }

83914dcf95836d8fd9eea57129b37db78595db61
schwester34 <yana64604@gmail.com>
2022-06-13 14:25:00 +0500

    added owner example

---
+++ src/test/java/tests/SystemPropertiesTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("systemProperties")
+public class SystemPropertiesTests {
+    @Test
+    void someTest3() {
+        String browser = System.getProperty("browser", "chrome");
+        System.out.println(browser);
+    }
+
+    @Test
+    void someTest4() {
+        CredentialsConfig credentialsConfig = ConfigFactory.create(CredentialsConfig.class);
+        String selenoidLogin = credentialsConfig.login();
+        String selenoidPassword = credentialsConfig.password();
+
+        System.out.println(selenoidLogin);
+        System.out.println(selenoidPassword);
+    }
+
+}
